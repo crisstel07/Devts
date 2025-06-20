939a793c2284c19cf019c8c94895b16d2dfafd57
crisstel07 <orozcocristel99@gmail.com>
2025-06-20 08:34:35 -0600

    Saving changes

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -15,6 +15,7 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("Esto es una prueba");
+        System.out.println("A");
         // TODO code application logic here
     }
     
